a30efe4e5f14d999379bd4742e7e1d8b431e4b8d
amckasson90@gmail.com <amckasson90@gmail.com>
2021-06-28 10:21:50 -0400

    Third Commit Restaurant Data added WebConfig updated

---
+++ RestaurantRestockPro.Data/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantRestockPro.Data
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantId { get; set; }
+        
+        [Required]
+        public Guid OwnerId { get; set; }
+        
+        [Required]
+        [Display(Name = "Restaurants Name")]
+        public string RestaurantName { get; set; }
+        
+        [Required]
+        [Display(Name = "Restaurants Location")]
+        public string RestaurantLocation { get; set; }
+    }
+}
