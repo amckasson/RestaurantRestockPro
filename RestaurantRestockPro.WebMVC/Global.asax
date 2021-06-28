91607d47ba732e630e081ad1d28e08a9dd8a709a
amckasson90@gmail.com <amckasson90@gmail.com>
2021-06-28 08:42:16 -0400

    Initital Commit Solution Setup

---
+++ RestaurantRestockPro.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestaurantRestockPro.WebMVC.MvcApplication" Language="C#" %>
---
+++ RestaurantRestockPro.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRestockPro.WebMVC.Startup))]
+namespace RestaurantRestockPro.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
