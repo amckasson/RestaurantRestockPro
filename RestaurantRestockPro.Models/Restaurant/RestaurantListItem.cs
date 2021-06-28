3fbe755c4cace0236e66d2ea6a406da87365997f
amckasson90@gmail.com <amckasson90@gmail.com>
2021-06-28 10:41:41 -0400

    Restaurant Model added Restaurant Controller added RestaurantListItem View added

---
+++ RestaurantRestockPro.Models/Restaurant/RestaurantListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantRestockPro.Models.Restaurant
+{
+    public class RestaurantListItem
+    {
+        public int RestaurantId { get; set; }
+
+        [Required]
+        [Display(Name = "Restaurants Name")]
+        public string RestaurantName { get; set; }
+    }
+}
