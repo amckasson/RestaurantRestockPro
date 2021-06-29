8cc7819c66fc50938399734fac79ae913c2dafa7
amckasson90@gmail.com <amckasson90@gmail.com>
2021-06-29 12:09:33 -0400

    StockOrder data added

---
+++ RestaurantRestockPro.Data/StockOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantRestockPro.Data
+{
+    public class StockOrder
+    {
+        [Key]
+        public int StockOrderId { get; set; }
+
+        [Required]
+        public string Manager { get; set; }
+
+        [Required]
+        public DateTimeOffset OrderCreated { get; set; }
+
+        public DateTimeOffset OrderUpdated { get; set; }
+
+        public decimal TotalCost { get; set; }
+    }
+}
