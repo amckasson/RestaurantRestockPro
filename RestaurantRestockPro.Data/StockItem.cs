f8541c8a94eaf389130d49d550ab144a17930f21
amckasson90@gmail.com <amckasson90@gmail.com>
2021-06-30 16:08:05 -0400

    StockItem added to IdentityModels and Data

---
+++ RestaurantRestockPro.Data/StockItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantRestockPro.Data
+{
+    public enum ItemType { Produce, Meat, Drygood, Other}
+    public class StockItem
+    {
+        [Key]
+        public int StockItemId { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public decimal Price { get; set; }
+        
+        [Required]
+        public bool IsFood { get; set; }
+
+        [Required]
+        public ItemType ItemType { get; set; }
+    }
+}
